aa16b18bbf4aa5b9340d73718207efdb2fbb14d0
Kiều Nam Thành <132132880+ThanhKieu249@users.noreply.github.com>
2025-03-13 13:27:33 +0700

    TH2

---
+++ TH2/DHT11/USER/main.c
@@ -0,0 +1,18 @@
+#include "stm32f10x.h"
+#include "dht11.h"
+#include "timer.h"
+#include "uart.h"
+#include "stdio.h"
+
+int main() {
+    UART_Init();
+    timer2_Init();
+		char buffer[50];
+		uint8_t humidity, temperature;
+    while (1) {
+				DHT11_Init(&humidity,&temperature);
+				sprintf(buffer, "Temperature: %d*C, Humidity: %d%%\n", temperature, humidity);
+        UART_SendString(buffer);
+        Delay_1ms(1000);
+    }
+}
